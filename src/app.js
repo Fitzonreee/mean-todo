035d5359f2d15501ef7cf309b914bcf9a20140a5
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-03-04 08:06:37 -0800

    Set up server and static file path

---
+++ src/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.use('/', express.static('public'));
+
+app.listen(3000, function() {
+  console.log("Up and running on port 3000...");
+})
+
+// nodemon src/app.js
+
+// NEED to do Angular ToDo List and clone repo
