e036665badc5140de336de1a03bf1257bed626c8
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-03-19 16:51:07 -0700

    Modularized and fake back end data in todos.json

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var express = require('express');
+var router = require('./api');
+
 var app = express();
 
 app.use('/', express.static('public'));
+
+app.use('/api', router);
 
 app.listen(3000, function() {
   console.log("Up and running on port 3000...");
