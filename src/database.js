8021e5b45b46dcd92645ae8ade59ce841519a399
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-03-23 07:57:43 -0700

    Removed Angular CDN link from index file, installed it with npm during webpack portion of course

--- src/database.js
+++ src/database.js
@@ -2,4 +2,10 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/mean-todo');
+mongoose.connect('mongodb://localhost/mean-todo', function(err) {
+  if (err) {
+    console.log("Failed to connect to mongodb!");
+  } else {
+    console.log("Successfully connected to mongodb!");
+  }
+});
