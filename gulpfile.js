a2a529c2ca51afae803b682f159525f9d5e87fa9
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-03-28 08:38:57 -0700

    All css files are being bundled into one file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concatCss = require('gulp-concat-css');
 
 gulp.task('sass', function(){
   return gulp.src('client/styles/scss/*.scss')
@@ -7,7 +8,19 @@ gulp.task('sass', function(){
     .pipe(gulp.dest('client/styles/css'))
 });
 
-//Watch task
+// Watch task
 gulp.task('watch',function() {
     gulp.watch('client/styles/scss/*.scss',['sass']);
 });
+
+// concatCss
+gulp.task('build', function () {
+  return gulp.src('client/styles/css/*.css')
+    .pipe(concatCss("_output/bundle.css"))
+    .pipe(gulp.dest('client/styles'));
+});
+
+// currently have to to run these tasks individually
+// gulp sass
+// gulp watch
+// gulp build
