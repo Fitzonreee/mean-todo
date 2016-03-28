2cf2aa1ec46c0aca38f7c669012d1925ad23d2d8
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-03-28 07:56:00 -0700

    Gulp watch sass task configured, need to build .scss files to one .css file

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-  return gulp.src('client/styles/scss/main.scss')
+  return gulp.src('client/styles/scss/*.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest('client/styles/css'))
+});
+
+//Watch task
+gulp.task('watch',function() {
+    gulp.watch('client/styles/scss/*.scss',['sass']);
 });
